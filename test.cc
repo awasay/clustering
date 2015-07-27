169375a8cd00f82304cda375690a4f0821e5745c
ppanteliadis <ppanteliad@gmail.com>
2015-07-27 12:16:01 -0400

    Updated test.cc

--- test.cc
+++ test.cc
@@ -1,29 +1,60 @@
-
-
 #include "data.h"
 #include "./tools/run_perf.hh"
 #include "./tools/timer.hh"
 
 using namespace std;
 
-int main(){
+
+
+int main(int argc, char* argv[]){
+	cout<<endl<<endl<<endl;
+	int N, K, dims;
+
+    N = atoi(argv[1]);
+    K = atoi(argv[2]);
+    dims = atoi(argv[3]);
+
+    string test_file_name = argv[4];
 
 	timer* t = new timer();
-	perfProfiler* p = new perfProfiler("cycles,cache-misses",false);
-
+	perfProfiler* p = new perfProfiler("cycles,cache-misses", false);
+	
 	t->start();
 	p->startPerf();
-	Data* d = new Data();
-	d->LoadData("test.data",3,3);
-	d->initialize(2);
-	d->PrintData();
-	//printf("%f\n",d->GetDataPoint(0).values);
-	d->AddClusterCenters(0);
-	d->AddClusterCenters(1);
-	printf("%f\n",d->GetDistance(0,1));
-	t->end();
-	sleep(5);
-	p->endPerf();
 
+	//sleep(2);
+
+
+	//1. Uncomment for k-means
+	/*
+	Data* d = new Data();
+	d->LoadData(test_file_name, N, dims);
+
+	d->initialize_Cluster_Centers(K);
+	d->AssignToCenters();
+	d->PrintAllDistances();
+	d->PrintData();
+	*/
+
+	//2. Uncomment for k-means++
+	Data* dpp = new Data();
+	dpp->LoadData(test_file_name, N, dims);
+	dpp->allocate_space_for_cluster_centers_KPP(K);
+	dpp->carefull_seeding_KPP(K);
+	
+
+
+
+
+	/* commmon for both implementations*/
+	dpp->AssignToCenters();
+	//dpp->PrintData();
+	//d->PrintAllDistances();
+
+
+
+	t->end();
+	p->endPerf();
+	cout<<"\nTotal Running time: "<<t->getDiff()<<" seconds."<<endl;
 	return 0;
 }
