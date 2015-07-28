c8df07c602854314abdd36bea2ff13219c298fc9
ppanteliadis <ppanteliad@gmail.com>
2015-07-28 05:59:47 -0400

    Updated test.cc with Google guidelines for C++

--- test.cc
+++ test.cc
@@ -4,57 +4,71 @@
 
 using namespace std;
 
-
-
-int main(int argc, char* argv[]){
-	cout<<endl<<endl<<endl;
-	int N, K, dims;
-
-    N = atoi(argv[1]);
-    K = atoi(argv[2]);
-    dims = atoi(argv[3]);
-
-    string test_file_name = argv[4];
-
-	timer* t = new timer();
-	perfProfiler* p = new perfProfiler("cycles,cache-misses", false);
-	
-	t->start();
-	p->startPerf();
-
-	//sleep(2);
-
-
-	//1. Uncomment for k-means
-	/*
+void KMeans(string test_file_name, int n, int k, int dims) {
 	Data* d = new Data();
-	d->LoadData(test_file_name, N, dims);
-
-	d->initialize_Cluster_Centers(K);
-	d->AssignToCenters();
-	d->PrintAllDistances();
-	d->PrintData();
-	*/
-
-	//2. Uncomment for k-means++
-	Data* dpp = new Data();
-	dpp->LoadData(test_file_name, N, dims);
-	dpp->allocate_space_for_cluster_centers_KPP(K);
-	dpp->carefull_seeding_KPP(K);
 	
+	d->LoadData(test_file_name, n, dims);
 
-
-
-
-	/* commmon for both implementations*/
-	dpp->AssignToCenters();
-	//dpp->PrintData();
+	d->InitializeClusterCenters(k);
+	d->AssignToCenters();
+	
 	//d->PrintAllDistances();
+	//d->PrintData();
+}
+
+void KMeansPlusPlus(string test_file_name, int n, int k, int dims) {
+	Data* dpp = new Data();
+	
+	dpp->LoadData(test_file_name, n, dims);
+	dpp->AllocateSpaceForClusterCentersKPP(k);
+	
+	dpp->CarefullSeedingKPP(k);
+	dpp->AssignToCenters();
+
+	//dpp->PrintData();
+	//dpp->PrintAllDistances();
+}
 
 
 
-	t->end();
-	p->endPerf();
-	cout<<"\nTotal Running time: "<<t->getDiff()<<" seconds."<<endl;
-	return 0;
+int main(int argc, char* argv[]) {
+	cout<<"argc = "<<argc<<endl;
+	if (argc == 5) {
+
+		//Get the data from command line.
+		int n = atoi(argv[1]);
+		cout<<"n = "<<n<<endl;
+		int k = atoi(argv[2]);
+		cout<<"k = "<<k<<endl;
+		int dims = atoi(argv[3]);
+		cout<<"dims = "<<dims<<endl;
+		string test_file_name = argv[4];
+		cout<<"test_file_name = "<<test_file_name<<endl;
+
+
+		timer* t = new timer();
+		perfProfiler* p = new perfProfiler("cycles,cache-misses", false);
+
+		t->start();
+		p->startPerf();
+
+
+		//1. Uncomment for k-means
+		KMeans(test_file_name, n, k, dims);
+
+
+		//2. Uncomment for k-means++
+		//KMeansPlusPlus(test_file_name, n, k, dims);
+
+
+		t->end();
+		p->endPerf();
+
+		cout<<"\nTotal Running time: "<<t->getDiff()<<" seconds."<<endl;
+		return 0;
+	}
+	else {
+		cout<<"Incorrect number of arguments."<<endl;
+		exit(EXIT_FAILURE);
+	}
 }
